023e3ba7b84202d2c6ba266c3eea104aa65d7431
June <june.it@qq.com>
2024-08-24 11:18:39 +0800

    增加自定义键值测试代码

--- tests/MyStack.DistributedMessage4RabbitMQ.Consumer/SubscribeDataHandler.cs
+++ tests/MyStack.DistributedMessage4RabbitMQ.Consumer/SubscribeDataHandler.cs
@@ -2,10 +2,12 @@
 
 namespace MyStack.DistributedMessage4RabbitMQ.Consumer
 {
+    [Subscribe("ABC")]
     public class SubscribeDataHandler : IDistributedEventHandler
     {
         public async Task HandleAsync(object eventData, CancellationToken cancellationToken = default)
         {
+            Console.WriteLine("SubscribeData");
             await Task.CompletedTask;
         }
     }
--- tests/MyStack.DistributedMessage4RabbitMQ.Shared/SubscribeData.cs
+++ tests/MyStack.DistributedMessage4RabbitMQ.Shared/SubscribeData.cs
@@ -1,11 +1,5 @@
-﻿using Microsoft.Extensions.DistributedMessage4RabbitMQ;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MyStack.DistributedMessage4RabbitMQ.Shared
+﻿namespace MyStack.DistributedMessage4RabbitMQ.Shared
 {
-    [Subscribe("ABC")]
     public class SubscribeData
     {
     }
