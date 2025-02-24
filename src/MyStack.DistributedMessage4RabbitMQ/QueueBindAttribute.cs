e23c2f169c5d24fb2f48146354227ead9f3eec60
june.it <june.it@qq.com>
2025-02-24 14:17:24 +0800

    Add annotations and explanations

--- src/MyStack.DistributedMessage4RabbitMQ/QueueBindAttribute.cs
+++ src/MyStack.DistributedMessage4RabbitMQ/QueueBindAttribute.cs
@@ -3,27 +3,27 @@
 namespace Microsoft.Extensions.DistributedMessage4RabbitMQ
 {
     /// <summary>
-    /// 表示绑定队列特性
+    /// Represents the queue binding attribute.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class QueueBindAttribute : Attribute
     {
         /// <summary>
-        /// 队列名称
+        /// The name of the queue.
         /// </summary>
         public string? QueueName { get; set; }
         /// <summary>
-        /// 交换机名称
+        /// The name of the exchange.
         /// </summary>
         public string? ExchangeName { get; set; }
         /// <summary>
-        /// 路由键
+        /// The routing key.
         /// </summary>
         public string RoutingKey { get; private set; } = default!;
         /// <summary>
-        /// 初始化一个绑定队列
+        /// Initializes a queue binding.
         /// </summary>
-        /// <param name="routingKey">路由键</param>
+        /// <param name="routingKey">The routing key.</param>
         public QueueBindAttribute(string routingKey)
         {
             RoutingKey = routingKey;
